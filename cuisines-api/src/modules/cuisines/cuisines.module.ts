6de0a7ff9edc4e9c87eaa7b3af5a04ae40dcba37
Ayomitide OAJ <ayomitide007@gmail.com>
2025-03-20 20:36:12 +0100

    chore: get cuisines functionality

--- cuisines-api/src/modules/cuisines/cuisines.module.ts
+++ cuisines-api/src/modules/cuisines/cuisines.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CuisinesController } from './cuisines.controller';
 import { CuisinesService } from './cuisines.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cuisine } from './entities/cuisines.entity';
+import { SetMenu } from './entities/setMenu.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cuisine, SetMenu])],
   controllers: [CuisinesController],
-  providers: [CuisinesService]
+  providers: [CuisinesService],
 })
 export class CuisinesModule {}
--- cuisines-api/src/modules/cuisines/cuisines.service.ts
+++ cuisines-api/src/modules/cuisines/cuisines.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cuisine } from './entities/cuisines.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CuisinesService {}
+export class CuisinesService {
+  constructor(
+    @InjectRepository(Cuisine)
+    private cuisineRepository: Repository<Cuisine>,
+  ) {}
+
+  async getCuisines(): Promise<Cuisine[]> {
+    return this.cuisineRepository.find({
+      order: { numberOfOrders: 'DESC' },
+    });
+  }
+}
