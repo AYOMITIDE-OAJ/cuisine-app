0babfa68797ca09b3f832e143c4a617ed75ba57a
Ayomitide OAJ <ayomitide007@gmail.com>
2025-03-20 20:43:22 +0100

    feat: controller - getCuisines, getSetMenus & createCuisines

--- cuisines-api/src/modules/cuisines/cuisines.controller.ts
+++ cuisines-api/src/modules/cuisines/cuisines.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CuisinesService } from './cuisines.service';
+import { CreateCuisineDto } from './dto/cuisines.dto';
 
 @Controller('cuisines')
-export class CuisinesController {}
+export class CuisinesController {
+  constructor(private readonly cuisinesService: CuisinesService) {}
+
+  @Get()
+  async getCuisines() {
+    return this.cuisinesService.getCuisines();
+  }
+
+  @Get('set-menus')
+  async getSetMenus(@Query('slug') cuisineSlug?: string) {
+    return this.cuisinesService.getSetMenus(cuisineSlug);
+  }
+
+  @Post()
+  async createCuisine(
+    @Body(new ValidationPipe()) createCuisineDto: CreateCuisineDto,
+  ): Promise<any> {
+    return this.cuisinesService.createCuisine(createCuisineDto);
+  }
+}
